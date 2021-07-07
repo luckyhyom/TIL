67380dbe4f30c434106bd4424e56e511d4d49f03
luckyhyom <bs_khm@naver.com>
2021-07-08 00:30:45 +0900

    cors

--- node/node_/6-express/routerTest.js
+++ node/node_/6-express/routerTest.js
@@ -3,7 +3,14 @@ import postRouter from './router/post.js';
 import userRouter from './router/user.js';
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // REST API -> BODY
+app.use(express.urlencoded({extended:false})); //HTML Form -> BODY
+
+// app.get('/photo.jpg',(rqe,res,next)=>{fs.readFile('./public/')})...
+// public안의 모든 파일에 접근할수있게 함
+app.use(express.static('public')); 
+
+
 app.use('/post',postRouter);
 app.use('/user',userRouter);
 
