0f6d01270115b3fee8ed6ead83803539e7f1b9a8
luckyhyom <bs_khm@naver.com>
2022-06-22 23:59:56 +0900

    모듈삭제

---
+++ node/route.js
@@ -0,0 +1,12 @@
+/**
+ * /cats/nabi 는 정적이다. (정해져있다.)
+ * /:id는 동적인 라우터다.
+ * /:kind 라는 형태로
+ * /umbrellar/shorts
+ * /umbrellar/long
+ * 이런식으로도 되고..
+ * 
+ */
+app.get('/cats/:id', (req, res) => {
+
+});
