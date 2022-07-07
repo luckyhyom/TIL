a1224e5a37f50d00db28eb5bb288dc32dfa9ca87
luckyhyom <bs_khm@naver.com>
2022-07-08 00:56:13 +0900

    js (closure) : isShow 라는 변수에 접근할 수 없으므로 안전하다. (전역으로 두면 다른 곳에서 접근이 가능하다.)

---
+++ node/closure/closure.js
@@ -0,0 +1,19 @@
+const toggle = (function (fn) {
+    let isShow = true;
+    return function () {
+        if (isShow) fn();
+        isShow = !isShow;
+    };
+});
+const a = toggle(() => { console.log(0) });
+a();
+a();
+a();
+a();
+
+/**
+ * isShow 라는 변수에 접근할 수 없으므로 안전하다. (전역으로 두면 다른 곳에서 접근이 가능하다.)
+ * 
+ * 버튼같은 곳에도 적용 가능
+ * btn.onClick = toggle(() => { console.log(0) });
+ */
