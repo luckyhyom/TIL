f8c82d96b830def4a9beadb88d8bfb33526bce3c
luckyhyom <bs_khm@naver.com>
2023-01-01 22:08:20 +0900

    feat: scheduler can handle error

--- javascript/nodejs/scheduler.js
+++ javascript/nodejs/scheduler.js
@@ -1,11 +1,19 @@
 const delay = 1000;
+const delayForFail = 3000;
 let i = 0;
 
-function func() {
-    console.log(i++);
-    setTimeout(func, delay);
+function scheduler() {
+    try {
+        console.log(i++);
+        if (i % 4 === 0) throw new Error("Error!");
+        setTimeout(scheduler, delay);
+    } catch (error) {
+        console.error(error);
+        setTimeout(scheduler, delayForFail);
+    }
 }
-const scheduler = setTimeout(func, delay);
+
+setTimeout(scheduler, delay);
 
 //function job(func, delay) {
 //    func();
