4c4087b6be2becea5110762654f3b88acf4cca97
luckyhyom <bs_khm@naver.com>
2023-01-12 00:50:46 +0900

    주옥같은 함수안에서의 객체 수정

---
+++ javascript/Node.js-Design-Patterns-Third-Edition/02-the-module-system/02-module-loader/loader-question.js
@@ -0,0 +1,36 @@
+const obj = {
+  exports: {}
+}
+
+obj.exports.log = { func: 'log!' }
+
+function load(obj2, objProp) {
+  console.log(objProp)
+  obj2 = {} // 쉣이네.. 여기서 완전히 새로운 변수가 생기는건가.. 값의 재할당이 아니라
+  obj2.exports = {} // 이게 먼저 실행되면 진짜 매개변수로 들어온 객체의 값이 바뀜
+
+  // 이건.. objProp을 재할당하는게 아니라 전역에 새로운 변수를 만들기때문에 원본에 변화가 없다.
+  // let이나 const를 사용하면.. 스코프가 지역변수로 잡히기때문에 컴파일 단계에서 오류가 발생한다.
+  objProp = { test: 'fromLoadFunction' }
+
+  console.log(objProp)
+  objProp.fromLoad = { func: console.log(7) }
+}
+load(obj, obj.exports)
+
+console.log(obj)
+
+/**
+ * 여기서는 객체가 수정되는데 위에서는 왜 안되냐
+ */
+
+function f(o) {
+  o.message = '수정함'
+  o = { message: '할당함' }
+  console.log(o.message)
+}
+
+const o = { message: '초기값' }
+console.log(o.message)
+f(o)
+console.log(o.message)
